511303ad027340d6fbc914f6156eaf8eb77887b5
Renata Buczko <renatabuczko03@gmail.com>
2024-05-23 18:56:02 +0200

    Add files via upload

---
+++ src/main/java/controller/CostPlannerController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.scene.input.TouchEvent;
+
+public class CostPlannerController {
+    public void saveAs(TouchEvent touchEvent) {
+    }
+
+    public void save(TouchEvent touchEvent) {
+    }
+}
