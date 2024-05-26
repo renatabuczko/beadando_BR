1ca5ab702a7987403220619f9830ee60c43ffbf4
Renata Buczko <renatabuczko03@gmail.com>
2024-05-26 14:17:49 +0200

    Save & Save As hozzáadása

--- src/main/java/controller/CostPlannerController.java
+++ src/main/java/controller/CostPlannerController.java
@@ -1,10 +1,18 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.TouchEvent;
+import javafx.stage.FileChooser;
+import model.CostPlannerCalculator;
+import org.tinylog.Logger;
+
+import java.io.IOException;
 
 public class CostPlannerController {
     public TextField income;
@@ -21,9 +29,43 @@ public class CostPlannerController {
     public Button save;
     public PieChart pieChartofCosts;
 
-    public void saveAs(TouchEvent touchEvent) {
+
+private final CostPlannerCalculator costPlanner = new CostPlannerCalculator();
+
+
+    public void onQuit(ActionEvent actionEvent) {
+        Logger.info("Terminating");
+        Platform.exit();
     }
 
-    public void save(TouchEvent touchEvent) {
+    @FXML
+    public void save(ActionEvent actionEvent) {
+        if (costPlanner.getFilePath() != null) {
+            Logger.debug("Saving file");
+            try {
+                costPlanner.save();
+            } catch (IOException e) {
+                Logger.error(e, "Failed to save file");
+            }
+        } else {
+            performSaveAs();
+        }
+    }
+    @FXML
+     public void saveAs(ActionEvent actionEvent) {
+        performSaveAs();
+    }
+    private void performSaveAs() {
+        var fileChooser = new FileChooser();
+        fileChooser.setTitle("Save As");
+        var file = fileChooser.showSaveDialog(null);
+        if (file != null) {
+            Logger.debug("Saving file as {}", file);
+            try {
+                costPlanner.saveAs(file.getPath());
+            } catch (IOException e) {
+                Logger.error(e, "Failed to save file");
+            }
+        }
     }
 }
--- src/main/java/model/CostPlannerCalculator.java
+++ src/main/java/model/CostPlannerCalculator.java
@@ -1,8 +1,69 @@
 package model;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import org.tinylog.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class CostPlannerCalculator {
 
-//bills, generalcost, foodcost, healthcost,
+   //1. mentési folyamat a save és save as gombokhoz:
+   private final StringProperty filePath = new SimpleStringProperty(null);
+
+   private final StringProperty content = new SimpleStringProperty("");
+
+   private final BooleanProperty modified = new SimpleBooleanProperty(false);
+
+   public CostPlannerCalculator(){
+      content.addListener((observable, oldValue, newValue) -> {
+         modified.set(true);
+         Logger.debug("Content changed");
+      });
+   }
+   public StringProperty filePathProperty() {
+      return filePath;
+   }
+
+   public final String getFilePath() {
+      return filePath.get();
+   }
+
+   public StringProperty contentProperty() {
+      return content;
+   }
+
+   public final String getContent() {
+      return content.get();
+   }
+
+   public BooleanProperty modifiedProperty() {
+      return modified;
+   }
+
+   public final boolean getModified() {
+      return modified.get();
+   }
+
+
+   public void save() throws IOException {
+      if (filePath.get() == null) {
+         throw new IllegalStateException();
+      }
+      saveAs(filePath.get());
+   }
+
+   public void saveAs(String filePath) throws IOException {
+      Files.writeString(Path.of(filePath), content.get());
+      this.filePath.set(filePath);
+      modified.set(false);
+   }
+
+
    public void addAllCost(String bills){
        //TODO
    }
