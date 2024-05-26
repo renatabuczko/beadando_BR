688967eb887bd398027079c13f36baf4482dc442
Renata Buczko <renatabuczko03@gmail.com>
2024-05-26 13:39:28 +0200

    Main + Application class
    Main és Application class-al kiegészítve.

---
+++ src/main/java/model/Main.java
@@ -0,0 +1,10 @@
+package model;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(CostPlannerApplication.class, args);
+    }
+
+}
